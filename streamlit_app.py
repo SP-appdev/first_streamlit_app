e5964ce7c6d59b9579039d6eaaa6d266331cfed3
SP-appdev <106947980+SP-appdev@users.noreply.github.com>
2022-06-06 12:07:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast menu')
 streamlit.text('Breakfast menu')
-                streamlit.text('omega 3')
+streamlit.text('omega 3')
