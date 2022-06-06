672cae52329cad362b3062da3ff11ccd339785e5
SP-appdev <106947980+SP-appdev@users.noreply.github.com>
2022-06-06 12:25:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeals')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-range Egg')
+
+streamlit.header('❤Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeals')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚Hard-boiled Free-range Egg')
+streamlit.text('🥑🍞Avocado toast')
+
+streamlit.header('🍇🥝Build Your Own Fruit Smoothies🍌🥭')
