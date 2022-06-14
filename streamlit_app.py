a4c213a29f569f0a2122d0a7fc96f04f11347dbd
SP-appdev <106947980+SP-appdev@users.noreply.github.com>
2022-06-14 17:34:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My parents new healthy dinner')
 
@@ -11,7 +15,7 @@ streamlit.text('🥑🍞Avocado toast')
 streamlit.header('🍇🥝Build Your Own Fruit Smoothies🍌🥭')
 
 
-import pandas
+#import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -22,7 +26,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
                       
 streamlit.dataframe(fruits_to_show)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
@@ -40,7 +44,7 @@ except URLError as e:
   streamlit.error()
 
 
-import snowflake.connector
+#import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
